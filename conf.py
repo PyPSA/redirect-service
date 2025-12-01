8a7483bedf048f914a212e9aae61b896e5c39ed3
Lukas Trippe <lkstrp@pm.me>
2025-12-01 10:10:14 +0100

    Add new redirects for documentation links

--- conf.py
+++ conf.py
@@ -20,5 +20,8 @@ html_static_path = []
 # -- Redirects configuration ------------------------------------------------
 redirects = {
     "report-bug/index": "https://github.com/PyPSA/PyPSA/issues/new?template=bug_report.yaml",
-    "examples/carbon-management.nc/index": "https://tubcloud.tu-berlin.de/s/3qZPGxW3r4HF9Hn/download?path=%2F&files=carbon-management.nc"
+    "examples/carbon-management.nc/index": "https://tubcloud.tu-berlin.de/s/3qZPGxW3r4HF9Hn/download?path=%2F&files=carbon-management.nc",
+    "new-components-api/index": "https://docs.pypsa.org/latest/user-guide/components/#new-components-class-api",
+    "options-params/index": "https://docs.pypsa.org/latest/user-guide/options/#parameters-options",
+    "release-notes/index:" "https://docs.pypsa.org/latest/release-notes/",
 }
